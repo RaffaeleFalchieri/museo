011fa5840a03deecf7ebb4bce43727ff10b6c55d
Raffaele Falchieri <raffaelefalchieri@10-20-49-134.0200.fed.it.iosda.org>
2017-12-06 12:43:56 +0100

    Initial Commit

---
+++ museo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  museo
+//
+//  Created by Raffaele Falchieri on 06/12/17.
+//  Copyright © 2017 Raffaele Falchieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
